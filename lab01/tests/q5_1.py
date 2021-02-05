b184b5f0e5e552ec7413e709d2295f69fe9c7d4f
nkoberstein <78602875+nkoberstein@users.noreply.github.com>
2021-02-05 10:01:04 -0500

    Delete lab01/tests directory

--- lab01/tests/q5_1.py
+++
@@ -1,3 +0,0 @@
-test = {   'name': 'q5_1',
-    'points': 1,
-    'suites': [{'cases': [{'code': '>>> round(paola_distance_from_average_m, 5)\n0.072', 'hidden': False, 'locked': False}], 'scored': True, 'setup': '', 'teardown': '', 'type': 'doctest'}]}
--- lab01/tests/q5_1_1.py
+++
@@ -1,3 +0,0 @@
-test = {   'name': 'q5_1_1',
-    'points': 1,
-    'suites': [{'cases': [{'code': '>>> round(min_height_difference, 5)\n0.04', 'hidden': False, 'locked': False}], 'scored': True, 'setup': '', 'teardown': '', 'type': 'doctest'}]}
