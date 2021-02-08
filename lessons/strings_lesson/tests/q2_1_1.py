8088118cf0fb8b5c60ce87da7422f4c0900de7ed
nkoberstein <78602875+nkoberstein@users.noreply.github.com>
2021-02-07 20:57:11 -0500

    Add files via upload

--- lessons/strings_lesson/tests/q2_1_1.py
+++ lessons/strings_lesson/tests/q2_1_1.py
@@ -1,10 +1,10 @@
-test = {   'name': 'q1_1_1',
+test = {   'name': 'q2_1_1',
     'points': 1,
-    'suites': [   {   'cases': [   {'code': ">>> 'phrase' in vars()\nTrue", 'hidden': False, 'locked': False},
-                                   {'code': '>>> phrase != ...\nTrue', 'hidden': False, 'locked': False},
+    'suites': [   {   'cases': [   {'code': ">>> 'combined_words' in vars()\nTrue", 'hidden': False, 'locked': False},
+                                   {'code': '>>> combined_words != ...\nTrue', 'hidden': False, 'locked': False},
                                    {   'code': '>>> # This is the correct answer, but instead of copying it;\n'
                                                '>>> # See if you can figure out how to calculate it;\n'
-                                               '>>> phrase == \'Welcome to NCSSM\\\'s\\n\\"Cryptography\\" Class.\'\n'
+                                               ">>> combined_words == 'quickbrownfox'\n"
                                                'True',
                                        'hidden': False,
                                        'locked': False}],
