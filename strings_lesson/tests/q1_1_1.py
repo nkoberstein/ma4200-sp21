59963550789e0560c1fc88ec93b7013671a350ee
nkoberstein <78602875+nkoberstein@users.noreply.github.com>
2021-02-07 20:56:31 -0500

    Add files via upload

---
+++ strings_lesson/tests/q1_1_1.py
@@ -0,0 +1,14 @@
+test = {   'name': 'q1_1_1',
+    'points': 1,
+    'suites': [   {   'cases': [   {'code': ">>> 'phrase' in vars()\nTrue", 'hidden': False, 'locked': False},
+                                   {'code': '>>> phrase != ...\nTrue', 'hidden': False, 'locked': False},
+                                   {   'code': '>>> # This is the correct answer, but instead of copying it;\n'
+                                               '>>> # See if you can figure out how to calculate it;\n'
+                                               '>>> phrase == \'Welcome to NCSSM\\\'s\\n\\"Cryptography\\" Class.\'\n'
+                                               'True',
+                                       'hidden': False,
+                                       'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
---
+++ strings_lesson/tests/q2_1_1.py
@@ -0,0 +1,14 @@
+test = {   'name': 'q2_1_1',
+    'points': 1,
+    'suites': [   {   'cases': [   {'code': ">>> 'combined_words' in vars()\nTrue", 'hidden': False, 'locked': False},
+                                   {'code': '>>> combined_words != ...\nTrue', 'hidden': False, 'locked': False},
+                                   {   'code': '>>> # This is the correct answer, but instead of copying it;\n'
+                                               '>>> # See if you can figure out how to calculate it;\n'
+                                               ">>> combined_words == 'quickbrownfox'\n"
+                                               'True',
+                                       'hidden': False,
+                                       'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
