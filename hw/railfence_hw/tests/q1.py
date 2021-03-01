98f75e826573117748f48f7df2ec34fc18b4a143
nkoberstein <78602875+nkoberstein@users.noreply.github.com>
2021-03-01 12:37:11 -0500

    Add files via upload

--- hw/railfence_hw/tests/q1.py
+++ hw/railfence_hw/tests/q1.py
@@ -1,5 +1,5 @@
 test = {   'name': 'q1',
-    'points': 6,
+    'points': 1,
     'suites': [   {   'cases': [   {'code': '>>> # Your encryption code is wrong;\n>>> msg = \'secret message\';\n>>> railfence(msg,4)=="SMEET EGCES ARS"\nTrue', 'hidden': False, 'locked': False},
                                    {   'code': '>>> # Your decryption code is wrong;\n>>> msg = \'secret message\';\n>>> railfence("SESEE RTESG CMA",3,False) == "secretmessage"\nTrue',
                                        'hidden': False,
